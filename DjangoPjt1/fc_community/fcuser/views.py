056dcb1b40d90caab7301f81fcd2ba63e0d4551b
osori <hyeseong43@gmail.com>
2020-06-30 00:55:46 +0900

    사용자 이메일 카테고리 영역 추가 회원가입 페이지 추가

--- DjangoPjt1/fc_community/fcuser/views.py
+++ DjangoPjt1/fc_community/fcuser/views.py
@@ -10,19 +10,20 @@ def register(request):
 
     elif request.method == 'POST':
         username = request.POST.get('username', None)
+        useremail = request.POST.get('useremail', None)
         password = request.POST.get('password', None)
         re_password = request.POST.get('re-password', None)
-        print('=============================================================================')
          
         res_data = {}
 
-        if not (username and password and re_password): 
+        if not (username and useremail and password and re_password): 
             res_data['error'] = '모든 값을 입력하세요'
         if password != re_password:
             res_data['error'] = '비밀번호가 달라요.'
         else:
             fcuser = Fcuser(
             username  = username, 
+            useremail  = useremail, 
             password = make_password(password)
          )
          
