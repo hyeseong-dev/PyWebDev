38364fc82ab2c3d3d876e0366d88cd0e6c11e8be
osori <hyeseong43@gmail.com>
2020-07-02 18:16:38 +0900

    디렉토리 변경 및 파일 삭제 로그 기록 등록

--- DjangoBasic/fc_community/fc_community/settings.py
+++ DjangoBasic/fc_community/fc_community/settings.py
@@ -23,12 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'kl3z4ih^xj2!m5sa39=pr7ad%su#51f6i%01&26wym0efs(t(('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = [
-'osorimagu.pythonanywhere.com'
-
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -124,7 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-# STATICFILES_DIRS = [
-#     os.path.join(BASE_DIR, 'static'),
-# ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
